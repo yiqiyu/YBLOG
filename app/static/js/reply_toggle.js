69422e4968762fa4096e9867047f6dd897dab45b
Dante Yu <dante3@126.com>
2016-07-25 12:09:23 +0800

    improve display on reply and message board

--- app/static/js/reply_toggle.js
+++ app/static/js/reply_toggle.js
@@ -1,5 +1,4 @@
-$(document).ready(
-	function() {
+$(function() {
 		$("a.btn").click(
 			function(){
 				replyToggle(this);
@@ -13,13 +12,14 @@ $(document).ready(
 function replyToggle(that){
 	if (!$('div.replyToggle').length){
 		$(that).parent().append("<div class='replyToggle' style='display:none'></div>");
-		Sijax.request('ajax_reply', [parseInt($(that).attr("id"))]);	
-		$('div.replyToggle')
-		    .delay(100)
-			.slideDown("fast", function(){
-			    $('input[type="checkbox"]')[0].checked = true;
-				$('textarea').autosize();
-			});
+		Sijax.request('ajax_reply', [+$(that).attr("id")], {'complete': function(){		
+		    $('div.replyToggle')
+		        //.delay(200)
+			    .slideDown("fast", function(){
+			        $('input[type="checkbox"]')[0].checked = true;
+				    $('textarea').autosize();
+			    })
+			}});   
 		}
 	else {
 		$('div.replyToggle').slideUp("fast", function(){$(this).remove()});
